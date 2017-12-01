cd6fbb3eaff682431189e0035fae099afe1da4f5
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-12-01 20:25:27 +1000

    throttled search term search

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
+import _ from 'lodash'
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
 import VideoDetail from './components/video_detail'
-import YTSearch from 'youtube-api-search'
 // youtube api key
 const API_KEY = 'AIzaSyBTsOLTDazv-tifx4rS7EvtAZl72YtKfTo'
 // create a new component and it should produce some html
@@ -16,8 +17,11 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     }
+    this.videoSearch('president trump')
+  }
 
-    YTSearch({key:API_KEY, term: "Surfboards"}, (videos) => {
+  videoSearch(term) {
+    YTSearch({key:API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -25,11 +29,13 @@ class App extends Component {
     })
   }
 
-
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300 )
+
+
     return(
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail  video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
